3542a639339bb0810a18893d87a5435e77cf6ffe
Tim lewis <tim_lewis@outlook.com>
2021-07-14 21:36:25 +0100

    undoing bad changes

--- src/index.js
+++ src/index.js
@@ -2,19 +2,31 @@ import React from "react";
 import ReactDOM from "react-dom";
 import './index.css';
 
-class Square extends React.Component {
-    render() {
-      return (
-        <button className="square">
-          {/* TODO */}
+function Square (props) {
+
+        return (
+        <button className="square" onClick={props.onClick}>
+          {props.value}
         </button>
       );
-    }
-  }
+      }
   
   class Board extends React.Component {
+    constructor(props){
+      super(props);
+      this.state = {
+        squares: Array(9).fill(null),
+      };
+this.handleClick = this.handleClick.bind(this)
+
+    }
+    handleClick(i){
+      const squares = this.state.squares.slice();
+      squares[i] = 'x';
+      this.setState({squares: squares});
+    }
     renderSquare(i) {
-      return <Square />;
+      return <Square value={this.state.squares[i]} onClick={()=> this.handleClick(i)}/>;
     }
   
     render() {
